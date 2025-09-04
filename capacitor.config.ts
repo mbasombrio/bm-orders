993e0db7663de3803478eb0fe44248a0024e9910
Matias <matiasbasombrio@gmail.com>
2025-09-04 10:03:03 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'bm-orders',
+  webDir: 'www'
+};
+
+export default config;
