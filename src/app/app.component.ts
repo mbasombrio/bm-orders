7af82c61e1757b9c3bb6b8aaa68996155cbdc2d9
Matias <matiasbasombrio@gmail.com>
2025-09-07 01:22:17 -0300

    hide menu on login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
-
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterLink, RouterLinkActive, NavigationEnd } from '@angular/router';
 import { IonApp, IonContent, IonHeader, IonIcon, IonItem, IonLabel, IonList, IonMenu, IonMenuToggle, IonRouterOutlet, IonTitle, IonToolbar } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import { cubeOutline, homeOutline, listOutline, logOutOutline } from 'ionicons/icons';
 import { AuthService } from './services/auth.service';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -14,12 +14,14 @@ import { AuthService } from './services/auth.service';
   standalone: true,
   imports: [CommonModule, RouterLink, RouterLinkActive, IonApp, IonRouterOutlet, IonMenu, IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonMenuToggle, IonItem, IonIcon, IonLabel],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   public appPages = [
     { title: 'Inicio', url: '/home', icon: 'home-outline' },
     { title: 'Pedidos', url: '/orders', icon: 'list-outline' },
     { title: 'Artículos', url: '/data', icon: 'cube-outline' },
   ];
+
+  isLoginPage: boolean = false;
 
   constructor(
     private router: Router,
@@ -28,9 +30,16 @@ export class AppComponent {
     addIcons({ homeOutline, listOutline, cubeOutline, logOutOutline });
   }
 
+  ngOnInit() {
+    this.router.events.pipe(
+      filter((event): event is NavigationEnd => event instanceof NavigationEnd)
+    ).subscribe((event: NavigationEnd) => {
+      this.isLoginPage = event.urlAfterRedirects === '/login';
+    });
+  }
+
   logout() {
     this.authService.logout();
     this.router.navigate(['/login']);
   }
 }
-
