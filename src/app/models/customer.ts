9178b802e30782dbc77444d292b603f89036ff7b
Matias <matiasbasombrio@gmail.com>
2026-03-20 11:51:06 -0300

    fix: resolve order serialization and history price display issues

--- src/app/models/customer.ts
+++ src/app/models/customer.ts
@@ -3,84 +3,84 @@ import { User } from './user';
 
 export const ivaSituationConsumidorFinal = 'CONSUMIDOR_FINAL';
 export class Customer {
-	id?: number;
-	dni?: string;
-	name?: string;
-	lastName?: string;
-	email?: string;
-	cellphone?: string;
-	address?: string;
-	zipCode?: string;
-	city?: string;
-	checkingAccountEnabled?: boolean;
-	password?: string;
-	totalRewardPoints?: number;
-	listPrice?: number;
-	user?: User;
-	alternativePhone?: string;
-	district?: string;
-	state?: string;
-	preferedContactTime?: string;
-	enabled?: boolean;
-	branch?: Branch;
-	saldoFavor?: number;
-	userName?: string;
-	observation?: string;
-	birthdayDate?: Date;
-	ivaSituation?: string;
-  status: string;
-	ctaCteLimitAmount: number;
-	customerType?: string;
+  id?: number;
+  dni?: string;
+  name?: string;
+  lastName?: string;
+  email?: string;
+  cellphone?: string;
+  address?: string;
+  zipCode?: string;
+  city?: string;
+  checkingAccountEnabled?: boolean;
+  password?: string;
+  totalRewardPoints?: number;
+  listPrice?: number;
+  user?: User;
+  alternativePhone?: string;
+  district?: string;
+  state?: string;
+  preferedContactTime?: string;
+  enabled?: boolean;
+  branch?: Branch;
+  saldoFavor?: number;
+  userName?: string | null;
+  observation?: string | null;
+  birthdayDate?: Date | null;
+  ivaSituation?: string | null;
+  status: string | null;
+  ctaCteLimitAmount: number | null;
+  customerType?: string | null;
+  lastModified?: Date | null;
 
-
-	constructor() {
-		this.id = undefined;
-		this.dni = undefined;
-		this.name = undefined;
-		this.lastName = undefined;
-		this.email = undefined;
-		this.cellphone = undefined;
-		this.address = undefined;
-		this.zipCode = undefined;
-		this.city = undefined;
-		this.checkingAccountEnabled = undefined;
-		this.password = '';
-		this.totalRewardPoints = undefined;
-		this.listPrice = 1;
-		this.user = new User();
-		this.alternativePhone = undefined;
-		this.district = undefined;
-		this.state = undefined;
-		this.preferedContactTime = undefined;
-		this.enabled = true;
-		this.branch = new Branch();
-		this.saldoFavor = 0;
-		this.userName = undefined;
-		this.observation = undefined;
-		this.birthdayDate = undefined;
-		this.ivaSituation = ivaSituationConsumidorFinal;
+  constructor() {
+    this.id = undefined;
+    this.dni = undefined;
+    this.name = undefined;
+    this.lastName = undefined;
+    this.email = undefined;
+    this.cellphone = undefined;
+    this.address = undefined;
+    this.zipCode = undefined;
+    this.city = undefined;
+    this.checkingAccountEnabled = undefined;
+    this.password = '';
+    this.totalRewardPoints = undefined;
+    this.listPrice = 1;
+    this.user = new User();
+    this.alternativePhone = undefined;
+    this.district = undefined;
+    this.state = undefined;
+    this.preferedContactTime = undefined;
+    this.enabled = true;
+    this.branch = new Branch();
+    this.saldoFavor = 0;
+    this.userName = undefined;
+    this.observation = undefined;
+    this.birthdayDate = undefined;
+    this.ivaSituation = ivaSituationConsumidorFinal;
     this.status = '';
-		this.ctaCteLimitAmount = 0;
-		this.customerType = undefined;
-	}
+    this.ctaCteLimitAmount = 0;
+    this.customerType = undefined;
+  }
 }
 
 export class CustomerFilter {
-	public name: string;
-	public lastname: string;
-	public dni: string;
-	public checking_account_enable: boolean;
-	public onlyenabled: boolean;
-	public birth_month: string;
+  public name: string;
+  public lastname: string;
+  public dni: string;
+  public checking_account_enable: boolean;
+  public onlyenabled: boolean;
+  public birth_month: string;
   public page: number;
 
-	constructor() {
-		this.name = '';
-		this.lastname = '';
-		this.dni = '';
-		this.checking_account_enable = false;
-		this.onlyenabled = true;
-		this.birth_month = 'TODOS';
+  constructor() {
+    this.name = '';
+    this.lastname = '';
+    this.dni = '';
+    this.checking_account_enable = false;
+    this.onlyenabled = true;
+    this.birth_month = 'TODOS';
     this.page = 1;
-	}
+  }
 }
